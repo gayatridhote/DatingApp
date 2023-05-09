a9ec2abb99ef5f8f3e1a4031f27c4efa8afd803d
Gayatri Dhote <gayatridhote996@gmail.com>
2023-05-09 17:40:08 +0530

    basic in api

---
+++ app/api/Data/DataContext.cs
@@ -0,0 +1,15 @@
+
+
+using api.entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
