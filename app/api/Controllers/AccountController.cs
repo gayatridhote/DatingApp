98d841ba61f7e1ea1fb21c08468766a57e3896cd
Gayatri Dhote <gayatridhote996@gmail.com>
2023-06-09 15:10:20 +0530

    routing and bootswatch theme

--- app/api/Controllers/AccountController.cs
+++ app/api/Controllers/AccountController.cs
@@ -52,7 +52,7 @@ namespace api.Controllers
         {
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
-            if (user==null) return  Unauthorized("Invalid password");
+            if (user==null) return  Unauthorized("Invalid username");
 
             using var hmac = new HMACSHA512(user.PasswordSalt);
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
